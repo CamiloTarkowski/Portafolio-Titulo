31a811b95e4ab4eca20131f5cc04a12febfd146a
camilo.tarkowski@gmail.com <camilo.tarkowski@gmail.com>
2022-05-23 12:33:20 -0400

    agregando nav-bar

--- portal-web/src/app/app.module.ts
+++ portal-web/src/app/app.module.ts
@@ -6,9 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormModule } from './form/form.module';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductoComponent } from './producto/producto.component';
+import { NavegadorComponent } from './navegador/navegador.component';
+import { FooterComponent } from './footer/footer.component';
+import { CatalogoComponent } from './catalogo/catalogo.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ProductoComponent, NavegadorComponent, FooterComponent, CatalogoComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
