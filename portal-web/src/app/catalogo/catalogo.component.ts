e28d15f5d609d3a446a3ff39ab95567dc6a63922
camilo.tarkowski@gmail.com <camilo.tarkowski@gmail.com>
2022-06-10 00:34:04 -0400

    consumiendo get de strapi en catalogo (implementacion)

--- portal-web/src/app/catalogo/catalogo.component.ts
+++ portal-web/src/app/catalogo/catalogo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductosService } from '../services/productos.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-catalogo',
@@ -8,14 +9,22 @@ import { ProductosService } from '../services/productos.service';
 })
 export class CatalogoComponent implements OnInit {
 
-  items:any[]=[];
+  items:any=[];
 
-  constructor(public productoService:ProductosService) {
+  constructor(public productoService:ProductosService, private _http:HttpClient) {
 
   }
 
   ngOnInit() {
+    var link = "http://localhost:1337/products";
+    this._http.get(link).subscribe(res=>{
+      this.items=res;
+      console.log('items en constructor: '+this.items);
+    })
     this.items = this.productoService.allItems;
+    console.log('Items: '+this.items);
+
+
   }
 
 }
--- portal-web/src/app/services/productos.service.ts
+++ portal-web/src/app/services/productos.service.ts
@@ -5,18 +5,9 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ProductosService {
-  
-  items:any[]=[
-    {"codigo":"101","nombre":"Pantalón de buzo","colegio":"Los Alerces","precio":19990,"imagen":"product1.jfif", "stock": 5, "talla": "L","descripcion": "Pantalón de buzo del colegio Los Alerces hecho de algodón"},
-    {"codigo":"102","nombre":"Buzo Completo","colegio": "Los Alerces","precio":29990,"imagen":"product2.jpeg", "stock": 5, "talla": "14", "descripcion": "Pantalón de buzo hecho de algodón"},
-    {"codigo":"103","nombre":"Pantalón de buzo","colegio": "Alcántara de Talagante","precio":15990,"imagen":"product3.jfif", "stock": 5, "talla": "16", "descripcion": "Pantalón de buzo rojo del colegio Alcántara de Talagante hecho de algodón."},
-    {"codigo":"104","nombre":"Pantalón de buzo","colegio":"Los Alerces","precio":19990,"imagen":"product1.jfif", "stock": 5, "talla": "L","descripcion": "Pantalón de buzo del colegio Los Alerces hecho de algodón"},
-    {"codigo":"105","nombre":"Buzo Completo","colegio": "Los Alerces","precio":29990,"imagen":"product2.jpeg", "stock": 5, "talla": "14", "descripcion": "Pantalón de buzo hecho de algodón"},
-    {"codigo":"106","nombre":"Pantalón de buzo","colegio": "Alcántara de Talagante","precio":15990,"imagen":"product3.jfif", "stock": 5, "talla": "16", "descripcion": "Pantalón de buzo rojo del colegio Alcántara de Talagante hecho de algodón."}
-  ]; 
-
+  items:any=[]; 
   constructor(private _http: HttpClient) {
-
+    
    }
 
    get allItems(){
