1144f1a378877b93ae458971aeeaa68436cd6fbc
Naitsab43 <bastianhuenuqueo43@gmail.com>
2022-06-16 14:23:41 -0400

    se arreglo lo que no mostraba las imagenes

--- portal-web/src/app/catalogo/catalogo.component.ts
+++ portal-web/src/app/catalogo/catalogo.component.ts
@@ -1,30 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductosService } from '../services/productos.service';
 import { HttpClient } from '@angular/common/http';
-
 @Component({
   selector: 'app-catalogo',
   templateUrl: './catalogo.component.html',
-  styleUrls: ['./catalogo.component.css']
+  styleUrls: ['./catalogo.component.css'],
 })
 export class CatalogoComponent implements OnInit {
+  products: any;
 
-  items:any=[];
-
-  constructor(public productoService:ProductosService, private _http:HttpClient) {
-
-  }
+  constructor(
+    public productoService: ProductosService,
+    private _http: HttpClient
+  ) {}
 
   ngOnInit() {
-    var link = "http://localhost:1337/products";
-    this._http.get(link).subscribe(res=>{
-      this.items=res;
-      console.log('items en constructor: '+this.items);
-    })
-    this.items = this.productoService.allItems;
-    console.log('Items: '+this.items);
-
-
+    var link = 'http://localhost:1337/products';
+    this._http.get(link).subscribe((products) => {
+      this.products = products;
+      console.log(this.products[0].image.url);
+      // console.log('items en constructor: ' + this.items);
+    });
+    // console.log('Items: ' + this.items);
   }
-
 }
